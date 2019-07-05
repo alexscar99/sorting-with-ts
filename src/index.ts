6123203c33b46a7066e629a70cf5c1fc58c6267d
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-05 03:14:41 -0400

    Refactored by removing interface and turning Sorter into an abstract class with LinkedList, NumbersCollection, and CharactersCollection classes inheriting from it

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const numbersSorter = new Sorter(numbersCollection);
-// numbersSorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('xPoZnbgaUtcsD');
-// const stringSorter = new Sorter(charactersCollection);
-// stringSorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('xPoZnbgaUtcsD');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -20,6 +18,5 @@ linkedList.add(-332);
 linkedList.add(41);
 linkedList.add(-8);
 
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.sort();
 linkedList.print();
