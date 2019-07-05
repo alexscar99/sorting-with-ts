3094c0a87e24cb19db81f11dfcbf5c0667c1f513
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-05 02:50:59 -0400

    Node and LinkedList classes written and tested

--- build/LinkedList.js
+++ build/LinkedList.js
@@ -1,6 +1,89 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Node = /** @class */ (function () {
+    function Node(data) {
+        this.data = data;
+        this.next = null;
+    }
+    return Node;
+}());
 var LinkedList = /** @class */ (function () {
     function LinkedList() {
+        this.head = null;
     }
+    LinkedList.prototype.add = function (data) {
+        var node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        var tail = this.head;
+        // When last node is reached, next will be undefined and we break out of loop
+        while (tail.next) {
+            tail = tail.next;
+        }
+        // Once at the last node, have it point to the new node
+        tail.next = node;
+    };
+    Object.defineProperty(LinkedList.prototype, "length", {
+        get: function () {
+            if (!this.head) {
+                return 0;
+            }
+            // Length will increment until last node is reached and 'node.next' is undefined
+            var length = 1;
+            var node = this.head;
+            while (node.next) {
+                length++;
+                node = node.next;
+            }
+            return length;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    LinkedList.prototype.at = function (index) {
+        if (!this.head) {
+            throw new Error('Index out of bounds');
+        }
+        var counter = 0;
+        var node = this.head;
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index out of bounds');
+    };
+    LinkedList.prototype.compare = function (leftIndex, rightIndex) {
+        if (!this.head) {
+            throw new Error('List is empty');
+        }
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    };
+    /*
+        Swapping nodes means you have to repair all the different references that
+        may be broken. For simplicity sake, this is swapping the values instead of the nodes.
+    */
+    LinkedList.prototype.swap = function (leftIndex, rightIndex) {
+        var leftNode = this.at(leftIndex);
+        var rightNode = this.at(rightIndex);
+        var leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    };
+    LinkedList.prototype.print = function () {
+        if (!this.head) {
+            return;
+        }
+        var node = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    };
     return LinkedList;
 }());
+exports.LinkedList = LinkedList;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const numbersSorter = new Sorter(numbersCollection);
 // numbersSorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('xPoZnbgaUtcsD');
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('xPoZnbgaUtcsD');
+// const stringSorter = new Sorter(charactersCollection);
+// stringSorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(142);
+linkedList.add(-332);
+linkedList.add(41);
+linkedList.add(-8);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
